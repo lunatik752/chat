ea9476f7a1ab1b840928731d0f9aa5878d27a57d
lunatik752 <whitemaxim752@gmail.com>
2021-01-19 20:31:17 +0300

    add UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import style from './App.module.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [messages, setMessage] = useState([
+        {message: 'Hello Nata', id: '123', user: {id: 'wqwer', name: 'Max'}},
+        {message: 'Hello Max', id: '321', user: {id: 'asdf', name: 'Nata'}}
+    ])
+
+    return (
+        <div className={style.App}>
+            <div className={style.container}>
+                {messages.map(m => {
+                    return <div>
+                        <b>{m.user.name}: </b> {m.message}
+                        <hr/>
+                    </div>
+                })}
+            </div>
+            <div className={style.formContainer}>
+                <textarea/>
+                <button>Send</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
